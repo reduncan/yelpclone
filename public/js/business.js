ff0152665d1fa4381770fd1b1a1d6fba44210021
Matthew C <matthew.j.carpenter1@gmail.com>
2018-10-05 22:20:40 -0400

    added google maps

---
+++ public/js/business.js
@@ -0,0 +1,21 @@
+$.ajax({
+url: '/api/restaurants'
+})
+
+
+
+function initMap() {
+    var uluru = { lat: 33.928694, lng: -84.343081 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: uluru,
+        tilt: 45,
+        disableDefaultUI: true
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
