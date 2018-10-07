c95a0a580c2c5ce75e70667615675e98f5867b03
pomyo <yeddev@gmail.com>
2018-10-07 15:09:43 -0400

    forgot to add test.js to push

---
+++ models/test.js
@@ -0,0 +1,13 @@
+// To simulate user input as a json
+
+const userInput = {
+    term: "salad",
+    tag: "Italian",
+    location: "Atlanta"
+}
+
+const scenarios = {
+    searchBy : userInput
+}
+
+module.exports = scenarios;
