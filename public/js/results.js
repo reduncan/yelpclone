a6cc4bc4122784ee5a110a432055b61a071884dd
Matthew C <matthew.j.carpenter1@gmail.com>
2018-10-09 20:41:22 -0400

    updated routes

--- public/js/results.js
+++ public/js/results.js
@@ -1,34 +1,34 @@
-  // const geocode = (e) => {
-  //   e.preventDefault();
-  //   let location = document.getElementById('locationInput').value
-  //   $.get('https://maps.googleapis.com/maps/api/geocode/json', {
-  //       params: {
-  //         address: location,
-  //         key: 'AIzaSyAQPXz579UmeXLiAqMxez-ud7xJJgnsxaI'
-  //       }
-  //     })
-  //     .then(function (res) {
-  //       console.log(res);
+// const geocode = (e) => {
+//   e.preventDefault();
+//   let location = document.getElementById('locationInput').value
+//   $.get('https://maps.googleapis.com/maps/api/geocode/json', {
+//       params: {
+//         address: location,
+//         key: 'AIzaSyAQPXz579UmeXLiAqMxez-ud7xJJgnsxaI'
+//       }
+//     })
+//     .then(function (res) {
+//       console.log(res);
 
-  //       let addressComponents = res.data.results[0].address_components;
+//       let addressComponents = res.data.results[0].address_components;
 
-  //       for (let i = 0; i < addressComponents.length; i++) {
-  //         addressComponentsOutput +=
-  //           `<li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>`;
-  //       }
-  //       console.log(addressComponents[i].types[0])
-  //       console.log(addressComponents[i].long_name)
-  //     })
+//       for (let i = 0; i < addressComponents.length; i++) {
+//         addressComponentsOutput +=
+//           `<li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>`;
+//       }
+//       console.log(addressComponents[i].types[0])
+//       console.log(addressComponents[i].long_name)
+//     })
 
-  //     .catch(function (err) {
-  //       console.log(err);
-  //     })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
 
-  // };
-  // geocode();
+// };
+// geocode();
 
 $(function () {
-  
+
   // Click listener for the submit button
   $('#submit').on('click', function (event) {
     event.preventDefault();
@@ -46,55 +46,55 @@ $(function () {
         });
         $('#holder').html(htmlstr);
       })
-      });
+  });
+});
 
-//GOOGLE MAPS INTERGRATION
-
-function initMap() {
+ //GOOGLE MAPS INTERGRATION
+ function initMap() {
   $.ajax({
-      url: '/api/restaurant',
-      method: 'GET',
-      dataType: 'json',
+    url: '/api/restaurant',
+    method: 'GET',
+    dataType: 'json',
   }).then(function (data) {
-      const latitude = data[0].coordinates.latitude;
-      const longitude = data[0].coordinates.longitude;
-      const name = data[0].name;         
-                      
-      const uluru = { lat: latitude, lng: longitude };
-      const map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          zoomControl: true,
-          zoomControlOptions: {
-              position: google.maps.ControlPosition.LEFT_TOP,
-          },
-          center: uluru,
-          tilt: 45,
-          disableDefaultUI: true
-      });
-      const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-          title: `${name}`
-      });
+    const latitude = data[0].coordinates.latitude;
+    const longitude = data[0].coordinates.longitude;
+    const name = data[0].name;
+    const uluru = { lat: latitude, lng: longitude };
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_TOP,
+      },
+      center: uluru,
+      tilt: 45,
+      disableDefaultUI: true
+    });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      title: `${name}`
+    });
+  }).catch(function(err){
+    console.log(err);
   })
 };
-const lessMoreToggle = function(){
-  if($('.map-header a span').text() === "Mo' Map"){
+const lessMoreToggle = function () {
+  if ($('.map-header a span').text() === "Mo' Map") {
     $('.map-header a span').text("Less Map");
   }
-  else{
+  else {
     $('.map-header a span').text("Mo' Map");
   }
 }
 
-const rotate = function(){
+const rotate = function () {
   $('.rotate').toggleClass('left');
 };
 
-$('.map-header a').on('click', function(e){
+$('.map-header a').on('click', function (e) {
   e.preventDefault();
   rotate();
   lessMoreToggle();
   $('.result-map').toggleClass('mo-map');
 })
-});
