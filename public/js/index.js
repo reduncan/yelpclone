c769d2317bb27040341e8c409c5b19038350b009
rduncan <rduncan@phpatlanta.com>
2018-10-11 16:07:37 -0400

    adds fucntion for randomly generating background

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,15 @@
-const db = require("../../models");
+const bgIndex = Math.floor((Math.random() * 10) + 1);
+
+const generate = function () {
+    $.ajax({
+        url: '/api/background',
+        method: 'GET',
+        dataType: 'json',
+    }).then (function (image) {
+        const newBG = image[bgIndex].url;
+        console.log(newBG)
+        $('body').css({"background-image": "url(${newBG})"});
+    });
+};
+
+window.onload = generate();
